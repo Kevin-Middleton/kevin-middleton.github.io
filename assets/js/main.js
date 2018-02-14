5cc74299691b1d372962379395dab85ad2ffc7ed
Kevin-Middleton <link2999@yahoo.com>
2018-02-14 16:48:06 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded",function(){new SweetScroll({});part
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
